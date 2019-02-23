a74ba6d16aae38d8a5e2d5e622111dd33c4d5f67
jagadisan8 <47880595+jagadisan8@users.noreply.github.com>
2019-02-24 01:09:34 +0530

    Create python3vowcons.py

---
+++ python3vowcons.py
@@ -0,0 +1,10 @@
+a=str(input())
+x=['a','e','i','o','u']
+y=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+if(a in x):
+    print("vowels")
+elif(a in y):
+    print("consonants")
+else:
+    print("invalid")
+    
